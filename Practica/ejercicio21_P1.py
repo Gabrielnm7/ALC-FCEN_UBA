ac474ce0c9283e0c3be4996615c5de6981263405
Gabrielnm7 <125508233+Gabrielnm7@users.noreply.github.com>
2024-08-22 08:54:29 -0300

    add ejercicio21_P1

---
+++ Practica/ejercicio21_P1.py
@@ -0,0 +1,64 @@
+"""
+Práctica 1 - Ejercicio 21
+"""
+
+import numpy as np
+
+def traza(A):
+    resp = 0
+    ##  CODIGO A COMPLETAR
+    for i in range(len(A)):
+        resp += A[i][i]
+    ## 
+    return resp
+
+def sumamodulo(A):
+    resp = 0
+    ##  CODIGO A COMPLETAR
+    for i in range(len(A)):
+        for j in range(len(A[i])):
+            resp += abs(A[i][j])
+    ## 
+    return resp
+
+
+def positivosmayoresanegativos(A):
+    resp = True
+    ##  CODIGO A COMPLETAR
+    positivos = 0
+    negativos = 0
+    for i in range(len(A)):
+        for j in range(len(A[i])):
+            if A[i][j] > 0:
+                positivos += 1
+            elif A[i][j] < 0:
+                negativos += 1
+    if positivos <= negativos:
+        resp = False    
+    ## 
+    return resp
+
+
+def main():
+
+    #Definición de matriz
+    A = np.array([[ 1, -2, -1, -2],
+                  [-4, -4,  5, -9],
+                  [-9,  0,  8, -1],
+                  [ 8,  3,  3, -3]])
+
+    print('Matriz A\n', A)
+    
+    print('Traza de A:', traza(A))
+    assert traza(A)==2
+    
+    print('Suma en módulo de elementos de A:', sumamodulo(A))
+    assert sumamodulo(A)==63
+    
+    print('Positivos mayores a negativos?:', positivosmayoresanegativos(A))
+    assert positivosmayoresanegativos(A)==False
+
+
+if __name__ == "__main__":
+    main()
+    
